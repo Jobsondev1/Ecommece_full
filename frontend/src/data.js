244f8b07b05e0df036196118702d95a16c38a19d
jobsondev1 <jammeloalves@gmail.com>
2021-08-27 18:31:50 -0300

    Exibir produtos

---
+++ frontend/src/data.js
@@ -0,0 +1,89 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'black-shoes',
+            category: 'tenis',
+            image: '/images/black-shoes.jpg',
+            price: 220,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: ' Novo tenis moderno'
+        },
+
+        {
+            _id: '2',
+            name: 'blue-jeans',
+            category: 'Calsa feminina',
+            image: '/images/blue-jeans.jpg',
+            price: 120,
+            brand: 'jensfamini',
+            rating: 5,
+            numReviews: 15,
+            description: ' jeans feminino elegante'
+        },
+
+        {
+            _id: '3',
+            name: 'brown-basic',
+            category: 'roupa',
+            image: '/images/brown-basic.jpg',
+            price: 70,
+            brand: 'NewStyle',
+            rating: 4.5,
+            numReviews: 8,
+            description: ' Blusa feminina preta descolada'
+        },
+
+        {
+            _id: '4',
+            name: 'brown-men',
+            category: 'tenis',
+            image: '/images/brown-men.jpg',
+            price: 300,
+            brand: 'Adidas',
+            rating: 5,
+            numReviews: 10,
+            description: '  Tenis Adidas esporte fino'
+        },
+
+        {
+            _id: '5',
+            name: 'gray-basic',
+            category: 'Calsa feninina',
+            image: '/images/gray-basic.jpg',
+            price: 200,
+            brand: 'new jens',
+            rating: 4.5,
+            numReviews: 12,
+            description: '  Calsa feminina jens azul'
+        },
+
+        {
+            _id: '6',
+            name: 'spring-jacket',
+            category: 'Jaqueta',
+            image: '/images/spring-jacket.jpg',
+            price: 350,
+            brand: 'boy',
+            rating: 4.5,
+            numReviews: 11,
+            description: 'jaqueta masculino '
+        },
+
+        {
+            _id: '7',
+            name: 'torn-jeans',
+            category: 'calsa jeans',
+            image: '/images/torn-jeans.jpg',
+            price: 250,
+            brand: 'boy',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Calsa masculina azul '
+        },
+    ]
+}
+
+export default data;
