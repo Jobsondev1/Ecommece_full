4ed1bb9a9836e8c76c31bc66cdea1dd821bca4f6
jobsondev1 <jammeloalves@gmail.com>
2021-09-10 16:10:38 -0300

    28. Implementar ação PlaceOrd

--- backend/src/routes/oderRouter.js
+++ backend/src/routes/oderRouter.js
@@ -5,27 +5,30 @@ import Order from '../model/orderModel.js';
 
 const orderRouter = expess.Router();
 
-orderRouter.post('/',
-isAuth,
-expressAsyncHandler(async (req,res)=>{
-    if (req.body.orderItems.length === 0){
-        res.status(400).send({ message: 'Carrinho está vazio'});
-    }else{
+orderRouter.post(
+    '/',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+      if (req.body.orderItems.length === 0) {
+        res.status(400).send({ message: 'Carrinho esta vazio' });
+      } else {
         const order = new Order({
-            orderItems: req.body.orderItems,
-            shippingAddress: req.body.shippingAddress,
-            paymentMethod: req.body.paymentMethod,
-            itemsPrice: req.body.itemsPrice,
-            shipingPrice: req.body.shipingPrice,
-            taxPrice: req.body.taxPrice,
-            totalPrice: req.body.totalPrice,
-            user: req.user._id,
-            });
-            const createOrder = await order.save();
-            res.status(201)
-            .send({ message: 'Nova order criada', order: createOrder});
-        }   
+          orderItems: req.body.orderItems,
+          shippingAddress: req.body.shippingAddress,
+          paymentMethod: req.body.paymentMethod,
+          itemsPrice: req.body.itemsPrice,
+          shippingPrice: req.body.shippingPrice,
+          taxPrice: req.body.taxPrice,
+          totalPrice: req.body.totalPrice,
+          user: req.user._id,
+        });
+        const createdOrder = await order.save();
+        res
+          .status(201)
+          .send({ message: 'Novo pedido criado', order: createdOrder });
+      }
     })
-);
+  );
+  
 
 export default orderRouter
--- frontend/src/screens/placeOrderScreen/PlaceOrderScreen.js
+++ frontend/src/screens/placeOrderScreen/PlaceOrderScreen.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
+import { createOrder } from '../../actions/orderActions';
+import LoadingBox from '../../components/loadingBox/LoadingBox';
+import MessageBox from '../../components/messageBox/MessageBox';
 import CheckoutSteps from '../../components/checkout/CheckoutSteps'
-const PlaceOrderScreen = (props) => {
-    const cart = useSelector((state)=> state.cart);
-    if (!cart.paymentMethod){
-        props.history.push('/payment');
-    }
+import { ORDER_CREATE_RESET } from '../../constants/orederConstants';
 
+const PlaceOrderScreen = (props) => {
+  const cart = useSelector((state) => state.cart);
+  if (!cart.paymentMethod) {
+    props.history.push('/payment');
+  }
+    const orderCreate = useSelector((state) => state.orderCreate);
+    const { loading, success, error, order } = orderCreate;
     const toPrice = (num) => Number(num.toFixed(2)); // 5.123 => "5.12" => 5.12
   cart.itemsPrice = toPrice(
     cart.cartItems.reduce((a, c) => a + c.qty * c.price, 0)
@@ -15,9 +21,16 @@ const PlaceOrderScreen = (props) => {
   cart.shippingPrice = cart.itemsPrice > 100 ? toPrice(0) : toPrice(10);
   cart.taxPrice = toPrice(0.15 * cart.itemsPrice);
   cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+  const dispatch = useDispatch();
   const placeOrderHandler = () => {
-    // TODO: dispatch place order action
+    dispatch(createOrder({ ...cart, orderItems: cart.cartItems }));
   };
+  useEffect(() => {
+    if (success) {
+      props.history.push(`/order/${order._id}`);
+      dispatch({ type: ORDER_CREATE_RESET });
+    }
+  }, [dispatch, order, props.history, success]);
     
     return (
         <div>
@@ -119,6 +132,8 @@ const PlaceOrderScreen = (props) => {
                   Faça a encomenda
                 </button>
               </li>
+              {loading && <LoadingBox></LoadingBox>}
+              {error && <MessageBox variant="danger">{error}</MessageBox>}
             </ul>
           </div>
         </div>
